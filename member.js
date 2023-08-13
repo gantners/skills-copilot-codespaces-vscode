373d45337587254810048b9ae63c3c1eaf19def4
Stefan Gantner <gantners@users.noreply.github.com>
2023-08-13 10:36:41 +0000

    update to member

--- member.js
+++ member.js
@@ -1,9 +1,3 @@
-function skillMember() {
-    var member = document.getElementById("member");
-    var memberValue = member.options[member.selectedIndex].value;
-    if (memberValue == "Yes") {
-        document.getElementById("skill").style.display = "block";
-    } else {
-        document.getElementById("skill").style.display = "none";
-    }
+function skillsMember() {
+    console.log('I am a member of skills')
 }
